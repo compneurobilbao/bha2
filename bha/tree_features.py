34a855c5f80ac3b0742f9e502a9b3262eed9aba7
a.jimenezmarin <antonio93jm@gmail.com>
2022-10-03 18:46:04 +0200

    code working correctly

--- bha/tree_features.py
+++ bha/tree_features.py
@@ -16,10 +16,14 @@ def connectome_average(fc_all, sc_all):
     return fcm, scm
 
 
-def tree_modules(g, fcm, scm, num_clust):
+def matrix_fusion(g, fcm, scm):
     cc = np.multiply(((g * abs(fcm)) + ((1 - g) * scm)), np.sign(fcm))
     cc_dist = pdist(cc, "cosine")
     W = cc_dist / max(cc_dist)
+    return W
+
+
+def tree_modules(W, num_clust):
     Z = linkage(W, "weighted")
     T = fcluster(Z, num_clust, criterion="maxclust")
     return T
@@ -27,18 +31,17 @@ def tree_modules(g, fcm, scm, num_clust):
 
 def level_dictionary(T):
     lvl = T.max()
-    t_dict = {}
+    l_dict = {}
     for i in range(1, np.max(T) + 1):
         rois_in_clust = np.where(T == i)[0]
         desc = "lvl_" + str(lvl) + "_mod_" + str(i)
-        t_dict[desc] = list(rois_in_clust)
-    return t_dict
+        l_dict[desc] = rois_in_clust.tolist()
+    return l_dict
 
 
-def compute_connectivity(fc, sc, T):
+def level_connectivity(fc, sc, T):
     lvl = T.max()
-    features = np.array([])
-    feat_names = np.array([])
+    level_features = pd.DataFrame()
 
     for i in range(1, np.max(T) + 1):
         rois_in_clust = np.where(T == i)[0]
@@ -47,78 +50,43 @@ def compute_connectivity(fc, sc, T):
         if len(rois_in_clust) > 1:
             desc = "lvl_" + str(lvl) + "_mod_" + str(i)
 
-            fc_int = fc.iloc[rois_in_clust][rois_in_clust].to_numpy().mean()
-            fc_out = fc.iloc[rois_in_clust][ext_rois].to_numpy().mean()
-            sc_int = (sc.iloc[rois_in_clust][rois_in_clust].to_numpy().sum()) / len(
-                rois_in_clust
-            )
-            sc_out = (sc.iloc[rois_in_clust][ext_rois].to_numpy().sum()) / len(
-                rois_in_clust
-            )
+            fc_int = fc.iloc[rois_in_clust][rois_in_clust].to_numpy().mean(dtype=float)
+            fc_out = fc.iloc[rois_in_clust][ext_rois].to_numpy().mean(dtype=float)
+            sc_int = (
+                sc.iloc[rois_in_clust][rois_in_clust].to_numpy().sum(dtype=float)
+            ) / len(rois_in_clust)
+            sc_out = (
+                sc.iloc[rois_in_clust][ext_rois].to_numpy().sum(dtype=float)
+            ) / len(rois_in_clust)
 
             features = pd.DataFrame(
-                np.concatenate((features, np.array([fc_int, fc_out, sc_int, sc_out])))
+                {
+                    "FCINT_" + desc: fc_int,
+                    "FCEXT_" + desc: fc_out,
+                    "SCINT_" + desc: sc_int,
+                    "SCEXT_" + desc: sc_out,
+                },
+                index=[0],
             )
-            feat_names = np.concatenate(
-                (
-                    feat_names,
-                    np.array(
-                        [
-                            "FCINT_" + desc,
-                            "FCEXT_" + desc,
-                            "SCINT_" + desc,
-                            "SCEXT_" + desc,
-                        ]
-                    ),
-                )
-            )
-            features.columns = feat_names
-    return features
+
+            level_features = pd.concat([level_features, features], axis=1)
+
+    return level_features
 
 
-# def generate_population_features(slist, g, num_clust_init, num_clust_end, scm, fcm):
-#     cc = np.multiply(((g * abs(fcm)) + ((1 - g) * scm)), np.sign(fcm))
-#     cc_dist = pdist(cc, "cosine")
-#     W = cc_dist / max(cc_dist)
-#     Z = linkage(W, "weighted")
+def tree_connectivity(init_level, end_level, W, sc, fc):
+    t_features = pd.DataFrame()
+    for i in range(init_level, end_level + 1):
+        T = tree_modules(W, i)
+        l_features = level_connectivity(fc, sc, T)
+        t_features = pd.concat([t_features, l_features], axis=1)
 
-#     X_features = slist.reshape(len(slist), 1)
-#     X_names = np.array(["label"])
-#     X_desc_dict = {}
-#     for num_clust in range(num_clust_init, num_clust_end + 1, 1):
-#         T = fcluster(Z, num_clust, criterion="maxclust")
-#         pfeatures = []
+    return t_features
 
-#         for i, sub in enumerate(slist):
-#             print(
-#                 "gamma = "
-#                 + str(g)
-#                 + ", lvl = "
-#                 + str(num_clust)
-#                 + ", snumber = "
-#                 + str(i)
-#             )
-#             sc = np.array(
-#                 pd.read_csv(
-#                     "sc_matrices/" + sub + "_anat_probabilistic_connectome.csv",
-#                     delimiter=" ",
-#                     header=None,
-#                 )
-#             )
-#             fc = np.corrcoef(
-#                 np.transpose(np.genfromtxt("timeseries/ts_" + sub + ".txt"))
-#             )
-#             scmod = np.log10(sc + 1)
-#             subfeatures, subfeat_names, feat_dict = compute_connectivity(
-#                 fc, scmod / scmod.max(), T, num_clust
-#             )
-#             pfeatures.append(subfeatures)
 
-#         X_features = np.hstack((X_features, np.array(pfeatures)))
-#         X_names = np.hstack((X_names, np.array(subfeat_names)))
-#         X_desc_dict.update(feat_dict)
-
-#     Xdf = pd.DataFrame(X_features)
-#     Xdf.columns = X_names
-#     Xdf = Xdf.loc[:, ~Xdf.apply(lambda x: x.duplicated(), axis=1).all()].copy()
-#     return Xdf, X_desc_dict
+def tree_dictionary(init_level, end_level, W):
+    t_dict = {}
+    for i in range(init_level, end_level + 1):
+        T = tree_modules(W, i)
+        t_dict.update(level_dictionary(T))
+    return t_dict
