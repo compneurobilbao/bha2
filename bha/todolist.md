4de1f9160fae88fd0c2d12572e3656edfc6d5aab
a.jimenezmarin <antonio93jm@gmail.com>
2023-01-17 14:17:48 +0100

    adding network measures

---
+++ bha/todolist.md
@@ -0,0 +1,5 @@
+# To do list
+1. Calculate between-module similarity also
+2. Plot accumulated Q vs. gamma
+3. Look to degree maps without building the tree for every gamma
+4. subdivide tree_features functions
--- bha/tree_features.py
+++ bha/tree_features.py
@@ -10,7 +10,7 @@ from scipy.spatial.distance import pdist
 import nibabel as nib
 import json
 from numba import njit, jit, prange
-
+import networkx as nx
 
 def connectome_average(fc_all, sc_all):
     fcm = np.median(fc_all, axis=0) - np.diag(np.diag(np.median(fc_all, axis=0)))
@@ -43,17 +43,79 @@ def level_dictionary(T):
     return l_dict
 
 
+def tree_dictionary(init_level, end_level, W, tree_class = "reduced"):
+    t_dict = {}
+    for i in range(init_level, end_level + 1):
+        T = tree_modules(W, i)
+        level_dict = level_dictionary(T)
+        for mod in level_dict:
+            rois = level_dict[mod]
+            if tree_class == "reduced":
+                if rois not in list(t_dict.values()):
+                    t_dict.update({mod: rois})
+            elif tree_class == "full":
+                t_dict.update({mod: rois})
+            else:
+                raise ValueError("tree_class must be 'reduced' or 'full'")
+    return t_dict
+
+
+
 def get_module_matrix(matrix, rois):
     module_matrix = matrix[rois, :][:, rois]
     return module_matrix
 
 
-def threshold_based_similarity(fcm, scm, tree):
+def level_from_tree(tree, level_num):
+    level_keys = list(filter(lambda x: x.startswith('lvl_' + str(level_num) + '_'), tree.keys()))
+    level = [tree[key] for key in level_keys]
+    labels = [key for key in level_keys]
+    return level, labels
+
+
+def T_from_level(level):
+    T = np.zeros(max(map(max, level))+1)
+    for idx, mod in enumerate(level):
+        T[mod] = idx+1
+    return T
+
+def adj_matrices_from_level(sc, fc, level, fc_type='pos', thresh=0.2):
+    reduced_sc = np.zeros((len(level), len(level)))
+    reduced_fc = np.zeros((len(level), len(level)))
+    if fc_type == 'abs':
+        fc_mod = np.abs(fc)
+    elif fc_type == 'sign':
+        fc_mod = fc
+    elif fc_type == 'pos':
+        fc_mod = np.where(fc > 0, fc, 0)
+    elif fc_type == 'thr':
+        fc_mod = np.where(np.abs(fc) > thresh, fc, 0)
+    for i, int_rois in enumerate(level):
+        for j, ext_rois in enumerate(level):
+            if i == j:
+                reduced_sc[i, j] = 0.0
+                reduced_fc[i, j] = 0.0
+            else:
+                sc_out = np.nansum(sc[int_rois][:,ext_rois], dtype=float) / len(int_rois)
+                fc_out = np.nanmean(fc_mod[int_rois][:,ext_rois], dtype=float)
+                reduced_sc[i, j] = sc_out
+                reduced_fc[i, j] = fc_out
+    return reduced_sc, reduced_fc
+
+
+def network_from_level(sc, fc, level, labels):
+    reduced_sc, reduced_fc = adj_matrices_from_level(sc, fc, level)
+    node_dict = {idx: label for idx, label in enumerate(labels)}
+    G_sc = nx.relabel_nodes(nx.from_numpy_matrix(reduced_sc), node_dict, copy=False)
+    G_fc = nx.relabel_nodes(nx.from_numpy_matrix(reduced_fc), node_dict, copy=False)
+    return G_sc, G_fc
+
+
+def threshold_based_similarity(fcm, scm, level, labels):
 
     module_sim = []
     module_thr = {}
-    for levels in tree:
-        rois = tree[levels]
+    for rois, mod_label in zip(level, labels):
         if len(rois) > 1:
             mod_fc = get_module_matrix(fcm, rois)
             mod_sc = get_module_matrix(scm, rois)
@@ -72,7 +134,7 @@ def threshold_based_similarity(fcm, scm, tree):
                         similarities.append(thr_sim)
                         tresholds.append(np.array([thr_a, thr_b]))
             module_sim.append(np.array(similarities).max())
-            module_thr[levels] = tresholds[np.array(similarities).argmax()]
+            module_thr[mod_label] = tresholds[np.array(similarities).argmax()]
     return module_sim, module_thr
 
 
@@ -86,30 +148,64 @@ def modularity(A, T):
     return Q
 
 
-def cross_modularity(fc, sc, g, l):
-    W = matrix_fusion(g, fc, sc)
-    T = tree_modules(W, l)
-    level = level_dictionary(T)
-
-    sims, thrs = threshold_based_similarity(fc, sc, level)
+def cross_modularity(fc, sc, level, labels):
+    sims, thrs = threshold_based_similarity(fc, sc, level, labels)
+    T = T_from_level(level)
     mod_sc = modularity(sc, T)
     mod_fc = modularity(fc, T)
     crossmod = pow((np.array(sims).mean() * mod_sc * mod_fc), (1 / 3))
 
     return crossmod, thrs
 
+def network_measures(Graph):
+    strength = {node: 0 for node in Graph.nodes()}
+    for u, v, weight in Graph.edges(data='weight'):
+        strength[u] += weight
+        strength[v] += weight
+    betweenness = nx.betweenness_centrality(Graph, weight='weight')
+    clustering_coef = nx.clustering(Graph, weight='weight')
+    pathlength = dict(nx.shortest_path_length(Graph, weight='weight'))
+    node_avg_pathlength = {node: 0 for node in Graph.nodes()}
+    for node,plengths in pathlength.items():
+        node_avg_pathlength[node] = sum(plengths.values())
+    n_measures = {key: [strength[key], betweenness[key], 
+        clustering_coef[key], node_avg_pathlength[key]] 
+        for key in betweenness.keys()}
+    return n_measures
 
-def tree_dictionary(init_level, end_level, W):
-    t_dict = {}
-    for i in range(init_level, end_level + 1):
-        T = tree_modules(W, i)
-        level_dict = level_dictionary(T)
-        for mod in level_dict:
-            rois = level_dict[mod]
-            if rois not in list(t_dict.values()):
-                t_dict.update({mod: rois})
-    return t_dict
 
+def brain_maps_network_measures(tree, sc, fc, atlas, range_of_levels):
+    atlas_data = atlas.get_fdata()
+    strength_sc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    strength_fc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    betweenness_sc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    betweenness_fc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    c_coeff_sc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    c_coeff_fc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    p_length_sc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+    p_length_fc_vol = np.zeros(atlas_data[:,:,:,0].shape)
+
+    for lv in range_of_levels:
+        level, labels = level_from_tree(tree, lv)
+        sc_level_net, fc_level_net = network_from_level(sc, fc, level, labels)
+        n_measures_sc = network_measures(sc_level_net)
+        n_measures_fc = network_measures(fc_level_net)
+        for rois, mod_label in zip(level, labels):
+            module_vol = get_module_vol(atlas, rois)
+            strength_sc_vol = strength_sc_vol + module_vol*n_measures_sc[mod_label][0]
+            strength_fc_vol = strength_fc_vol + module_vol*n_measures_fc[mod_label][0]
+            betweenness_sc_vol = betweenness_sc_vol + module_vol*n_measures_sc[mod_label][1]
+            betweenness_fc_vol = betweenness_fc_vol + module_vol*n_measures_fc[mod_label][1]
+            c_coeff_sc_vol = c_coeff_sc_vol + module_vol*n_measures_sc[mod_label][2]
+            c_coeff_fc_vol = c_coeff_fc_vol + module_vol*n_measures_fc[mod_label][2]
+            p_length_sc_vol = p_length_sc_vol + module_vol*n_measures_sc[mod_label][3]
+            p_length_fc_vol = p_length_fc_vol + module_vol*n_measures_fc[mod_label][3]
+
+    sc_img_list = [strength_sc_vol, betweenness_sc_vol, c_coeff_sc_vol, p_length_sc_vol]
+    fc_img_list = [strength_fc_vol, betweenness_fc_vol, c_coeff_fc_vol, p_length_fc_vol]
+    return sc_img_list, fc_img_list
+            
+   
 
 def module_connectivity(rois, label, sc, fc):
     int_rois = np.array(rois)
@@ -163,11 +259,10 @@ def tree_connectivity(tree_dictionary, sc, fc):
     return t_features, t_features_names
 
 
-def get_module_img(atlas, rois, value=1):
+def get_module_vol(atlas, rois, value=1):
     atlas_data = atlas.get_fdata()
-    module_img = np.where(atlas_data == (np.array(rois) + 1), value, 0).sum(axis=3)
-    img = nib.Nifti1Image(module_img, affine=atlas.affine)
-    return img
+    module_vol = np.where(atlas_data == (np.array(rois) + 1), value, 0).sum(axis=3)
+    return module_vol
 
 
 def add_gamma_to_lvl_dict(dict, g):
